322c48ff7f753c11cb356cb21023e96a506daf72
Pabsalonso <56518151+Pabsalonso@users.noreply.github.com>
2022-04-28 20:04:14 +0100

    Mejora de rutas y sistema de autentificacion por token. Creado firewall, ruta de login y configuracion del token por lexik_jwt. Movido auth_controler al controlador de usuario

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -8,11 +8,26 @@ use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations;
 use Symfony\Component\HttpFoundation\Request;
-use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractFOSRestController{
+
+    /**
+     * @Annotations\Post(path="/register")
+     * @Annotations\View(serializerGroups={"user"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function register(Request $request, UserPasswordHasherInterface $encoder, UserRepository $userRepository)
+    {
+        $password = $request->get('password');
+        $email = $request->get('email');
+        $user = new User();
+        $user->setPassword($encoder->hashPassword($user, $password));
+        $user->setEmail($email);
+        $userRepository->add($user);
+        return $this->json([
+            'user' => $user->getEmail()
+        ]);
+    }
 
     /**
      * @Annotations\Get(path="/user")
@@ -35,20 +50,23 @@ class UserController extends AbstractFOSRestController{
      */
      public function getUserbyToken(Request $request, UserRepository $userRepository)
     {
-        try {
-            $token = $request->headers->get('X-AUTH-TOKEN');
-            $credentials = str_replace('Bearer ', '', $token);
-            $jwt = (array) JWT::decode(
-                $credentials,
-                new Key($this->getParameter('jwt_secret'),'HS256')
-            );
-            return $userRepository
-                    ->findOneBy([
-                            'email' => $jwt['email'],
-                    ]);
-        }catch (\Exception $exception) {
-                throw new AuthenticationException($exception->getMessage());
-        }
+        return $this->json([
+            'message' => 'test!',
+     ]);
+        // try {
+        //     $token = $request->headers->get('X-AUTH-TOKEN');
+        //     $credentials = str_replace('Bearer ', '', $token);
+        //     $jwt = (array) JWT::decode(
+        //         $credentials,
+        //         new Key($this->getParameter('jwt_secret'),'HS256')
+        //     );
+        //     return $userRepository
+        //             ->findOneBy([
+        //                     'email' => $jwt['email'],
+        //             ]);
+        // }catch (\Exception $exception) {
+        //         throw new AuthenticationException($exception->getMessage());
+        // }
     }
 
 }
